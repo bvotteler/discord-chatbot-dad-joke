6969ab127b514f3aed8c514b8936f7bb63435cbe
Brendon Votteler <bvotteler@users.noreply.github.com>
2021-12-15 20:16:40 +1100

    first attempt to add async handling of incoming discord slash command events. Not quite working just yet

--- src/handler.js
+++ src/handler.js
@@ -4,6 +4,9 @@ const axios = require('axios');
 const { InteractionResponseType, InteractionType, verifyKey } = require('discord-interactions');
 const getDiscordSecrets = require('./secrets/discordSecret.js').getDiscordSecrets;
 const apiKeyName = require('./secrets/discordSecret.js').apiKeyName;
+const { SQS } = require("aws-sdk");
+
+const sqs = new SQS();
 
 module.exports.hello = async (event) => {
   return {
@@ -86,5 +89,132 @@ module.exports.discordHandler = async (event, ctx, callback) => {
         type: InteractionResponseType.PONG
       })
     });
+  }
+};
+
+module.exports.endpoint = async (event, ctx, callback) => {
+  // TODO: clean up later when confident about event structure
+  console.log('event: ' + JSON.stringify(event, null, 2));
+
+  // get discord secrets
+  const discordSecrets = await getDiscordSecrets();
+
+  // Verify the request
+  const signature = event.headers['x-signature-ed25519'];
+  const timestamp = event.headers['x-signature-timestamp'];
+
+  const rawBody = event.body;
+  const isValidRequest = await verifyKey(rawBody, signature, timestamp, discordSecrets.CLIENT_PUBLIC_KEY);
+  if (!isValidRequest) {
+    console.error('Invalid request, return 401.');
+    return callback(null, {
+      statusCode: 401,
+      body: 'Bad request signature'
+    });
+  }
+
+  // Handle the payload
+  // TODO: safeguard json parse?
+  const interaction = JSON.parse(event.body);
+
+  if (interaction && interaction.type === InteractionType.APPLICATION_COMMAND) {
+    // got a command, assuming it is /dadjoke
+    console.log('Identified incoming command. Queuing it up to be consumed by sender method.');
+    let statusCode = 200;
+
+    try {
+      await sqs
+        .sendMessage({
+          QueueUrl: process.env.QUEUE_URL,
+          MessageBody: event.body,
+          MessageAttributes: {
+            AttributeName: {
+              StringValue: "Attribute Value",
+              DataType: "String",
+            },
+          },
+        })
+        .promise();
+  
+    } catch (error) {
+      console.log(error);
+      statusCode = 500;
+    }
+  
+    return {
+      statusCode,
+      body: JSON.stringify({
+        type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE
+      }),
+    };
+
+  } else {
+    console.log('Not an application command, assuming a ping. Return pong.');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        type: InteractionResponseType.PONG
+      })
+    };
+  }
+};
+
+module.exports.sender = async (event) => {
+  for (const record of event.Records) {
+    const messageAttributes = record.messageAttributes;
+    console.log(
+      "Message Attribute: ",
+      messageAttributes.AttributeName.stringValue
+    );
+    console.log("Message Body: ", record.body);
+
+    // grab the token and interaction id
+    // TODO: beware of risky parsing and assuming the token will be there. make more resilient.
+    const jsonBody = JSON.parse(record.body)
+    const interactionToken = jsonBody.token;
+    const interactionId = jsonBody.id;
+
+    // fetch joke from api
+    console.log('Fetching joke from API.');
+    const jokeApiResponse = await axios.get('https://icanhazdadjoke.com/', {
+      headers: {
+        'Accept': 'application/json',
+        'User-Agent': 'axios 0.23.0'
+      }
+    });
+
+    // parse response
+    let joke = 'Yeah... Not funny, but something didn\'t quite work.';
+    if (jokeApiResponse && jokeApiResponse.data && jokeApiResponse.data.joke) {
+      console.log('Successfully fetched joke from API.');
+      joke = jokeApiResponse.data.joke;
+    } else {
+      console.error('Failed to fetch joke from API.');
+    }
+
+
+    // send response to discord
+    const responseBody = {
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: joke
+      }
+    };
+
+    const url = `https://discord.com/api/v8/interactions/${interactionId}/${interactionToken}/callback`;
+
+    // const response = await axios.post(url, responseBody);
+
+    await axios.post(url, responseBody, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'User-Agent': 'axios 0.23.0'
+      }
+    })
+      .catch(err => {
+        console.log('error ->' + JSON.stringify(err, null, 2));
+      });
   }
 };
